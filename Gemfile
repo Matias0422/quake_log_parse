c43234269ee9f58f31007d50aae8e4c4886db034
Bruno Matias Casas <brunomatias0422@gmail.com>
2024-04-29 01:25:21 +0000

    Refact parse tree and quake parser logic

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 
 gem 'parslet'
 gem 'byebug'
+gem 'memory_profiler'
+
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'byebug'
 require 'parslet'
 require 'parslet/convenience'
 
@@ -6,51 +7,60 @@ require_relative 'death_couse_enum'
 
 class MatchParser < Parslet::Parser
   LOG_FILE_PATH = 'quake_log.txt'
-  CHUNK_SIZE = 1000
+  LINES_CHUNK = 100
   MATCH_DELIMITER_REGEX = /-{60}/
-  WORLD_NAME = '<world>'
+  WORLD_PLAYER_ID = 1022
 
   # Elements
+  rule(:space)  { match('\s').repeat(1) }
+  rule(:space?) { space.maybe }
   rule(:number) { match('[0-9]+').repeat(1) }
   rule(:timestamp) { number >> str(':') >> number }
   rule(:name) { match('\w[^\n]+\w').repeat(1) }
-  rule(:death_cause) { match('[' + DeathCouseEnum.constants.join('|') + ']').repeat(1) }
+  rule(:death_couse) { match('[' + DeathCouseEnum.constants.join('|') + ']').repeat(1) }
 
   # Lines
   rule(:init_game_line) {
-    str('InitGame: ') >>
-    match('.+').repeat(1)
+    str('InitGame: ')
   }
   rule(:kill_line) {
-    timestamp >>
-    str(' Kill: ') >>
+    str('Kill: ') >>
     number.as(:killer_id) >>
-    str(' ') >>
-    number >>
-    str(' ') >>
+    space >>
+    number.as(:victim_id) >>
+    space >>
     number >>
     str(': ') >>
     name.as(:killer_name) >>
     str(' killed ') >>
     name.as(:victim_name) >>
-    str(' by ') >>_
-    death_cause.as(:death_cause)
+    str(' by ') >>
+    death_couse.as(:death_couse)
   }
   rule(:player_connect_line) {
-    str('ClientConnect: ') >>
+    str('ClientConnect:') >>
+    space >>
     number.as(:player_id)
   }
   rule(:player_changed_line) {
-    str('ClientUserinfoChanged: ') >> 
-    number.as(:player_id) >> 
-    str('n\\') >>
-    name.as(:player_name) >>
-    match('.+').repeat(1)
+    str('ClientUserinfoChanged: ') >>
+    number.as(:player_id) >>
+    str(' n\\') >>
+    name.as(:player_name)
   }
 
   # Root
+  rule(:default_beginning) { space? >> timestamp >> space }
+  rule(:default_end) { match('.+').repeat(1).maybe }
   rule(:line) {
-    init_game_line.as(:init_game_line) | kill_line.as(:kill_line) | player_connect_line.as(:player_connect_line) | player_changed_line.as(:player_changed_line)
+    default_beginning >>
+    (
+      init_game_line.as(:init_game_line) |
+      kill_line.as(:kill_line) |
+      player_connect_line.as(:player_connect_line) |
+      player_changed_line.as(:player_changed_line)
+    ) >>
+    default_end
   }
   root :line
 
@@ -62,8 +72,8 @@ class MatchParser < Parslet::Parser
 
   def call
     File.open(LOG_FILE_PATH, "r") do |file|
-      while chunk = file.read(CHUNK_SIZE)
-        parse_chunk(chunk)
+      file.each_slice(LINES_CHUNK) do |lines|
+        parse_lines(lines)
       end
     end
 
@@ -72,20 +82,25 @@ class MatchParser < Parslet::Parser
 
   private
 
-  def parse_chunk(chunk)
-    chunk.lines do |line|
-      if line.match(MATCH_DELIMITER_REGEX) && @current_match
-        @match_counter += 1
-        @matches["game_#{@match_counter}".to_sym] = @current_match
-        @current_match = nil
-      else
-        parse_line(line)
-      end
+  def parse_lines(lines)
+    lines.each do |line|
+      next if line.match(MATCH_DELIMITER_REGEX) && @current_match.nil?
+      conclude_match && next if line.match(MATCH_DELIMITER_REGEX) && @current_match
+
+      parse_line(line)
     end
+  end
+
+  def conclude_match
+    @match_counter += 1
+    @matches["game_#{@match_counter}".to_sym] = @current_match
+    @current_match = nil
   end
 
   def parse_line(line)
     parse_tree = parse_with_debug(line)
+
+    return if parse_tree.nil?
 
     case(parse_tree.keys.last)
     when :init_game_line
@@ -105,8 +120,8 @@ class MatchParser < Parslet::Parser
     @current_match = {
       total_kills: 0,
       players: {},
-      kills_by_means: DeathCouseEnum.constants.map do |death_cause|
-         [death_cause, 0]
+      kills_by_means: DeathCouseEnum.constants.map do |death_couse|
+         [death_couse, 0]
       end.to_h
     }
   end
@@ -122,7 +137,7 @@ class MatchParser < Parslet::Parser
 
   def handle_player_changed_line(parse_tree)
     player_id = parse_tree[:player_changed_line][:player_id].to_i
-    player_name = parse_tree[:player_changed_line][:player_name]
+    player_name = parse_tree[:player_changed_line][:player_name].to_s
 
     @current_match[:players][player_id][:name] = player_name
   end
@@ -136,17 +151,20 @@ class MatchParser < Parslet::Parser
 
   def handle_player_kills(parse_tree)
     killer_id = parse_tree[:kill_line][:killer_id].to_i
+    victim_id = parse_tree[:kill_line][:victim_id].to_i
 
-    if killer_name == WORLD_NAME
-      @current_match[:players][killer_id][:kills] += 1
+    if killer_id == WORLD_PLAYER_ID
+      @current_match[:players][victim_id][:kills] -= 1
     else
-      @current_match[:players][killer_id][:kills] -= 1
+      @current_match[:players][killer_id][:kills] += 1
     end
   end
 
   def handle_kills_by_means(parse_tree)
-    death_cause = parse_tree[:kill_line][:death_cause].to_sym
+    death_couse = parse_tree[:kill_line][:death_couse].to_sym
 
-    @current_match[:kills_by_means][death_cause] += 1
+    @current_match[:kills_by_means][death_couse] += 1
   end
 end
+
+MatchParser.new.call
